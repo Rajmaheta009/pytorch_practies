c1f42a5ad7fc1aac39662f5936703d84864f1216
RaJ <rajmaheta2008@gmail.com>
2025-06-09 15:01:37 +0530

    save and add new file

---
+++ index_tensor_practical.py
@@ -0,0 +1,110 @@
+import torch
+
+# Sample tensor
+x = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
+y = torch.tensor([[5, 6], [7, 8]], dtype=torch.float32)
+z = torch.tensor([1, 0, 3])
+# Prepare source data of the same shape as the slice (2 elements in this case)
+src = torch.tensor([9.0, 8.0])
+
+# --- Tensor operations ---
+print("adjoint:\n", x.adjoint())  # Conjugate + transpose
+
+print("arg where:\n", torch.argwhere(torch.tensor([[0, 1], [1, 0]])))
+
+print("cat:\n", torch.cat([x, y], dim=1))
+
+print("concat:\n", torch.concat([x, y], dim=1))
+
+print("concatenate:\n", torch.concatenate([x, y], dim=1))
+
+print("conj:\n", torch.conj(x))
+
+print("chunk:\n", torch.chunk(torch.tensor([1, 2, 3, 4]), 4))
+
+print("dsplit:\n", torch.dsplit(torch.arange(18).reshape(3,2,3),3))
+
+print("column_stack:\n", torch.column_stack((z, z + 2)))
+
+print("dstack:\n", torch.dstack((z, z * 2)))
+
+print("gather:\n", torch.gather(x, 1, torch.tensor([[0, 1], [1, 0]])))
+
+print("hsplit:\n", torch.hsplit(torch.arange(6).reshape(2, 3), 3))
+
+print("hstack:\n", torch.hstack((z, z + 1)))
+
+print("index_add:\n", torch.index_add(torch.zeros(5), 0, torch.tensor([1, 3, 1]), torch.tensor([9.0, 10.0, 11.0])))
+
+print("index_copy:\n", torch.index_copy(torch.zeros(5), 0, torch.tensor([0, 2]), torch.tensor([5.0, 6.0])))
+
+print("index_reduce:\n", torch.index_reduce(torch.zeros(5), 0, torch.tensor([1, 1, 3]), torch.tensor([2.0, 3.0, 4.0]), reduce="amax"))
+
+print("index_select:\n", torch.index_select(torch.tensor([10, 20, 30, 40]), 0, torch.tensor([1, 3])))
+
+print("masked_select:\n", torch.masked_select(x, x > 2))
+
+print("movedim:\n", torch.movedim(torch.ones(2, 3, 4), 0, -1).shape)
+
+print("moveaxis:\n", torch.moveaxis(torch.ones(2, 3, 4), 0, -1).shape)
+
+print("narrow:\n", torch.narrow(x, 1, 0, 1))
+
+print("narrow_copy:\n", torch.narrow_copy(x, 1, 0, 1))
+
+print("nonzero:\n", torch.nonzero(torch.tensor([0, 1, 2, 0, 3])))
+
+print("permute:\n", torch.ones(2, 3, 4).permute(2, 0, 1).shape)
+
+print("reshape:\n", x.reshape(4))
+
+print("row_stack:\n", torch.vstack((z, z + 1)))
+
+print("select:\n", x.select(0, 1))
+
+print("scatter:\n", torch.scatter(torch.zeros(2, 4), 1, torch.tensor([[1], [2]]), torch.tensor([[9.0], [10.0]])))
+
+print("diagonal_scatter:\n", torch.diagonal_scatter(torch.zeros(3, 3), torch.tensor([1., 2., 3.])))
+
+print("select_scatter:\n", torch.select_scatter(torch.zeros(2, 3),src, 1,1))
+
+print("slice_scatter:\n", torch.slice_scatter(torch.zeros(2, 3), torch.tensor([[1., 2., 3.]]), dim=0, start=0, end=1))
+
+print("scatter_add:\n", torch.scatter_add(torch.zeros(2, 4), 1, torch.tensor([[1], [2]]), torch.tensor([[9.0], [10.0]])))
+
+print("scatter_reduce:\n", torch.scatter_reduce(torch.zeros(2, 4), 1, torch.tensor([[1], [2]]), torch.tensor([[9.0], [10.0]]), reduce="sum"))
+
+print("split:\n", torch.split(torch.tensor([1, 2, 3, 4]), 2))
+
+print("squeeze:\n", torch.squeeze(torch.tensor([[[1]]])))
+
+print("stack:\n", torch.stack((z, z + 1), dim=0))
+
+print("swapaxes:\n", torch.swapaxes(torch.ones(2, 3), 0, 1))
+
+print("swapdims:\n", torch.swapdims(torch.ones(2, 3), 0, 1))
+
+print("t:\n", torch.tensor([[1, 2], [3, 4]]).t())
+
+print("take:\n", torch.take(torch.tensor([[1, 2], [3, 4]]), torch.tensor([0, 2])))
+
+print("take_along_dim:\n", torch.take_along_dim(torch.tensor([[10, 20], [30, 40]]), torch.tensor([[0, 0], [1, 0]]), dim=1))
+
+print("tensor_split:\n", torch.tensor_split(torch.arange(10), 3))
+
+print("tile:\n", torch.tile(torch.tensor([0, 1, 2]), (2,)))
+
+print("transpose:\n", torch.transpose(torch.ones(2, 3), 0, 1))
+
+print("unbind:\n", torch.unbind(torch.tensor([[1, 2], [3, 4]]), dim=0))
+
+print("unravel_index:\n", torch.unravel_index(torch.tensor([2, 5, 7]), (3, 3)))
+
+print("unsqueeze:\n", torch.unsqueeze(torch.tensor([1, 2, 3]), 0))
+
+print("vsplit:\n", torch.vsplit(torch.arange(6).reshape(3, 2), 3))
+
+print("vstack:\n", torch.vstack((z, z + 1)))
+
+print("where:\n", torch.where(torch.tensor([True, False, True]), torch.tensor([1, 2, 3]), torch.tensor([4, 5, 6])))
+
--- tensor_practical.py
+++ tensor_practical.py
@@ -65,6 +65,102 @@ torch.set_default_dtype(torch.float64)
 a = torch.tensor([1.0, 2.0])
 print(a.dtype)  # torch.float64
 
-# Constructs a sparse tensor in COO(rdinate) format with specified values at the given indices.
-x = torch.sparse_coo_tensor(x1,x2,size=(10,))
-print(x)
+# --- Basic Tensors ---
+t = torch.tensor([[1.0, 2.0], [3.0, 4.0]])
+print("tensor:\n", t)
+
+# --- Sparse Tensors ---
+x1 = torch.tensor([[0, 1], [1, 2]])
+x2 = torch.tensor([3.0, 4.0])
+sparse_coo = torch.sparse_coo_tensor(x1, x2, size=(3, 3))
+print("sparse_coo_tensor:\n", sparse_coo)
+
+data = [1, 2, 3, 4]
+crow_indices = torch.tensor([0, 2, 4])
+col_indices = torch.tensor([0, 1, 1, 2])
+sparse_csr = torch.sparse_csr_tensor(crow_indices, col_indices, torch.tensor(data))
+print("sparse_csr_tensor:\n", sparse_csr)
+
+ccol_indices = torch.tensor([0, 2, 4])
+row_indices = torch.tensor([0, 1, 1, 2])
+sparse_csc = torch.sparse_csc_tensor(ccol_indices, row_indices, torch.tensor(data))
+print("sparse_csc_tensor:\n", sparse_csc)
+
+block_data = torch.tensor([[1, 2], [3, 4], [5, 6], [7, 8]])
+bsr = torch.sparse_bsr_tensor(crow_indices, col_indices, block_data.view(2, 2, 2))
+print("sparse_bsr_tensor:\n", bsr)
+
+bsc = torch.sparse_bsc_tensor(ccol_indices, row_indices, block_data.view(2, 2, 2))
+print("sparse_bsc_tensor:\n", bsc)
+
+# --- Conversions ---
+arr = [1, 2, 3]
+asarr = torch.asarray(arr)
+print("asarray:\n", asarr)
+
+astens = torch.as_tensor(arr)
+print("as_tensor:\n", astens)
+
+x = torch.tensor([1, 2, 3, 4])
+as_strided_view = torch.as_strided(x, size=(2,), stride=(2,))
+print("as_strided:\n", as_strided_view)
+
+# from_numpy
+np_array = np.array([[1, 2], [3, 4]])
+from_np = torch.from_numpy(np_array)
+print("from_numpy:\n", from_np)
+
+# frombuffer
+buffer = bytearray([1, 0, 0, 0, 2, 0, 0, 0])
+from_buf = torch.frombuffer(buffer, dtype=torch.int32)
+print("frombuffer:\n", from_buf)
+
+# from_dlpack (mock example, normally from another lib)
+# Not executed here, would require real DLPack data
+# from_dlpack = torch.from_dlpack(dlpack_tensor)
+
+# --- Initialization ---
+print("zeros:\n", torch.zeros(2, 3))
+print("zeros_like:\n", torch.zeros_like(t))
+
+print("ones:\n", torch.ones(2, 3))
+print("ones_like:\n", torch.ones_like(t))
+
+print("arange:\n", torch.arange(0, 10, 2))
+print("range:\n", torch.range(0, 5, 1))  # deprecated in favor of arange
+
+print("linspace:\n", torch.linspace(0, 1, steps=5))
+print("logspace:\n", torch.logspace(1, 2, steps=5, base=10.0))
+
+print("eye:\n", torch.eye(3))
+print("empty:\n", torch.empty(2, 2))
+print("empty_like:\n", torch.empty_like(t))
+print("empty_strided:\n", torch.empty_strided((2, 3), (1, 2)))
+
+print("full:\n", torch.full((2, 2), 7))
+print("full_like:\n", torch.full_like(t, 9))
+
+# --- Quantized ---
+float_tensor = torch.tensor([[1.0, 2.0], [3.0, 4.0]])
+q_tensor = torch.quantize_per_tensor(float_tensor, scale=0.1, zero_point=10, dtype=torch.quint8)
+print("quantize_per_tensor:\n", q_tensor)
+
+per_channel_q = torch.quantize_per_channel(float_tensor, scales=torch.tensor([0.1, 0.2]), zero_points=torch.tensor([0, 0]), axis=0, dtype=torch.qint8)
+print("quantize_per_channel:\n", per_channel_q)
+
+dequant = q_tensor.dequantize()
+print("dequantize:\n", dequant)
+
+# --- Complex ---
+real = torch.tensor([1.0, 2.0])
+imag = torch.tensor([3.0, 4.0])
+complex_tensor = torch.complex(real, imag)
+print("complex:\n", complex_tensor)
+
+polar_tensor = torch.polar(torch.tensor([1.0, 2.0]), torch.tensor([0.0, 3.14]))
+print("polar:\n", polar_tensor)
+
+# --- Heaviside ---
+x = torch.tensor([-1.5, 0, 2.0])
+y = torch.tensor([1.0, 0.5, 0.0])
+print("heaviside:\n", torch.heaviside(x, y))
